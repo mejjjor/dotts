a186448094022c1ef9a40981a9c4e4cd323ec94b
Erik Aouizerate <erik.aouizerate-ext@mfi.fr>
2024-10-23 15:40:41 +0200

    rr

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 sudo dnf install -y git ansible
-# git clone https://github.com/mejjjor/dotts.git
+if [ -d "dotts" ]; then
+    echo "Directory 'dotts' already exists."
+else
+    git clone https://github.com/mejjjor/dotts.git
+fi
 cd dotts
 ansible-playbook  -i ./inventories/localhost  setup.yml
